5fb48aef7839538e03f4dd862df475ac194b9738
Damian Arquez <demian_ms@live.cl>
2018-08-12 01:18:38 -0300

    Add styling

--- src/FilteredList/FilteredList.js
+++ src/FilteredList/FilteredList.js
@@ -7,12 +7,13 @@ import './FilteredList.css'
 
 const Result = (props) => {
   const redirectToNetflix = (uri) => {
+    console.log(uri);
     chrome.tabs.query({currentWindow: true, active: true}, (tab) => {
       chrome.tabs.update(tab.id, {url: uri});
-    })
+    });
   };
   return (
-    <li>
+    <li className='result'>
       <button
         children={props.item}
         key={props.item.itemId}
@@ -28,7 +29,8 @@ class FilteredList extends React.Component {
     super(props);
     this.state = {
       currentQuery: '',
-      items: []
+      items: [],
+      loading: false
     }
   }
 
@@ -36,27 +38,27 @@ class FilteredList extends React.Component {
     let results;
     if (this.state.items.length !== 0) {
       results =
-      <ul>
+      <ul className='results'>
         {
           this.state.items.map(function(item) {
             return <Result item={item.name} itemId={item.id} uri={item.uri} key={item.id} />
           })
         }
       </ul>;
+    } else if (this.state.loading) {
+      results = <div className='loader-container'><div className='lds-ripple'><div></div><div></div></div></div>
     } else {
-      results = <div>Realiza una busqueda!</div>;
+      results = <div className='empty-list circle'>Realiza una busqueda!</div>;
     }
     return (
       <div className="filter-list">
         <form onSubmit={(event) => this.submitHandler(event)}>
-          <fieldset>
-            <input
-              type="text"
-              placeholder="¿Qué quieres ver?"
-              onChange={(event) => this.filterList(event)}
-              className="search-bar"
-            />
-          </fieldset>
+          <input
+            type="text"
+            placeholder="¿Qué quieres ver?"
+            onChange={(event) => this.filterList(event)}
+            className="search-bar"
+          />
         </form>
         {results}
       </div>
@@ -67,27 +69,31 @@ class FilteredList extends React.Component {
     const word = event.target.value;
     this.setState({
       currentQuery: word,
-      items: this.state.items
+      items: this.state.items,
+      loading: true
     })
 
     // Empty word => Message instead of results
     if (word === '') {
       this.setState({
         currentQuery: word,
-        items: []
+        items: [],
+        loading: false
       })
       return;
     }
 
     // Query the API and set results unless the word has changed
-    const query = '?=' + word;
+    const query = '?w=' + word + '&limit=10';
     const response = await API.get(query);
     if (this.state.currentQuery !== word) {
       return;
     }
+    console.log(response.data);
     this.setState({
       items: response.data.slice(0, this.props.length),
-      currentQuery: word
+      currentQuery: word,
+      loading: false
     })
   }
 
